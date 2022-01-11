3e95beff88f5c76c26ae31b54f8fa758df14f155
Martin Lenart <martin_lenart@icloud.com>
2022-01-11 17:02:08 +0100

    Add project files.

---
+++ Hello World Tabbed/Hello World Tabbed/Views/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+//using Hello_World_Tabbed.Models;
+
+namespace Hello_World_Tabbed.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
